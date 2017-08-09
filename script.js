8c9e3f52fb5bdbb2ecbf00045b10dc21a5bcd5b3
lusijanka <lucjamiodek@gmail.com>
2017-08-09 16:38:04 +0200

    index.html completed, script.js corrected

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 window.onload = function() {
 	console.log('The page is fully loaded.');	
 	var buttons = document.getElementsByClassName('button');
-	for (i = 0; i <= buttons.length; i++) {
-		message.innerHTML = 'New hello!';
-		buttons.appendChild(message);
-		//alert (message);
+	for (var i = 0; i < buttons.length; i++) {
+		alert(buttons[i].innerText);
 	};
 };
